37323c6bd6efc99ffe05db143a6a6a19a7a87043
Benlin Gan <benlingan@gmail.com>
2024-08-26 23:54:58 -0500

    fix dataset loading

--- data.py
+++ data.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-def gen_dataset(path, sensitive_attribute):
-    df = pd.read_csv(path, chunk_size=100000)
-
+def gen_dataset(path, sensitive_attribute, toxic_threshold=0.15):
+    dfg = pd.read_csv(path, chunksize=100000) 
+    for df in dfg:
+        df = df[(df['split'] == 'train') & (~df[sensitive_attribute].isna())]
+        df['sensitive'] = df[sensitive_attribute] > 0.0
+        df['toxic'] = df["toxicity"] > toxic_threshold
+        yield df[['id', 'comment_text', 'toxic', 'sensitive']]
+    
 
